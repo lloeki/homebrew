ac296f60b44a9972952194d894bcd4a51507320b
Loic Nageleisen <loic.nageleisen@gmail.com>
2013-11-07 17:01:38 +0100

    Fix duplicity to Just Work
    This way its bin/*:
    
    - use system python, not one arbitrarily installed with say pyenv
    - have a proper sys.path/PYTHONPATH set

--- Library/Formula/duplicity.rb
+++ Library/Formula/duplicity.rb
@@ -5,17 +5,37 @@ class Duplicity < Formula
   url 'http://code.launchpad.net/duplicity/0.6-series/0.6.22/+download/duplicity-0.6.22.tar.gz'
   sha1 'afa144f444148b67d7649b32b80170d917743783'
 
-  depends_on :python
   depends_on 'librsync'
   depends_on 'gnupg'
 
   option :universal
 
+  # TODO: Move this into Library/Homebrew somewhere (see also mitmproxy.rb).
+
+  def wrap bin_file, pythonpath
+    puts bin_file
+    bin_file = Pathname.new bin_file
+    libexec_bin = Pathname.new libexec/'bin'
+    libexec_bin.mkpath
+    mv bin_file, libexec_bin
+    puts libexec_bin
+    bin_file.write <<-EOS.undent
+      #!/bin/sh
+      PYTHONPATH="#{pythonpath}:$PYTHONPATH" "#{libexec_bin}/#{bin_file.basename}" "$@"
+    EOS
+  end
+
+  def pythonpath
+    "#{lib}/python2.7/site-packages"
+  end
+
   def install
-    python do
-      ENV.universal_binary if build.universal?
-      # Install mostly into libexec
-      system python, "setup.py", "install", "--prefix=#{prefix}"
+    ENV.universal_binary if build.universal?
+
+    system "python", "setup.py", "install", "--prefix=#{prefix}"
+
+    Dir["#{bin}/*"].each do |bin_file|
+      wrap bin_file, pythonpath
     end
   end
 end
